17c4dc98c2fd66eb76e1d270281531b20cccb15c
davidsnts <davidwallacedossantos@gmail.com>
2023-09-26 13:43:03 -0300

    deploy prod

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const mysql2 = require('mysql2');
 
+// const db = mysql2.createConnection({
+//     host: 'mysql.davidsantos.kinghost.net',
+//     user: 'davidsantos',
+//     password: '101522mg',
+//     port: 3306,
+//     database: 'davidsantos',
+// });
+
 const db = mysql2.createConnection({
-    host: 'mysql.davidsantos.kinghost.net',
-    user: 'davidsantos',
-    password: '101522mg',
+    host: 'mysql.geracaoeleita.kinghost.net',
+    user: 'geracaoeleita',
+    password: 'ge2023jf',
     port: 3306,
-    database: 'davidsantos',
+    database: 'geracaoeleita',
 });
 
 // try {
